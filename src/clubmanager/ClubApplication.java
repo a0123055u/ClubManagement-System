6ced3fe4709615d642de3752b086b0796ff4516b
a0123055u <m.thiruvalluvar@gmail.com>
2014-11-19 12:15:20 +0800

    Booking class with modifications

--- src/clubmanager/ClubApplication.java
+++ src/clubmanager/ClubApplication.java
@@ -1,5 +1,10 @@
 package clubmanager;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class ClubApplication {
 
 
@@ -8,20 +13,32 @@ public static void main(String args[])
 
 	Club z=  new Club();
 	//z.getNumber(j);
-	z.addMember("Murugesan","Thiru","valluvan");
-	z.addMember("Renganathan", "Gowtham", "Karthick");
-	z.addMember("R","shiva","Niranjan");
+//	z.addMember("Murugesan","Thiru","valluvan");
+//	z.addMember("Renganathan", "Gowtham", "Karthick");
+//	z.addMember("R","shiva","Niranjan");
+//	
+//	//z.showMember();
+//	//z.removeMember(1);
+//	//z.showMember();
+//	//z.removeMember(2);
+//	//	z.showMember();
+//	z.addFacility("Cricket"," 5 kit are available");
+//	z.addFacility("Badmitton", "10 bat are available");
+//	z.addFacility("Volley ball", "10 balls are available");
+//	//z.showfacilities();
+//	//	z.getFacility("Cricket");
+//	z.show();
 	
-	//z.showMember();
-	//z.removeMember(1);
-	//z.showMember();
-	//z.removeMember(2);
-//	z.showMember();
-z.addFacility("Cricket"," 5 kit are available");
-z.addFacility("Badmitton", "10 bat are available");
-z.addFacility("Volley ball", "10 balls are available");
-//z.showfacilities();
-//	z.getFacility("Cricket");
-	z.show();
+	DateFormat df = new SimpleDateFormat("dd.mm.yyyy");
+	Date start= Calendar.getInstance().getTime();
+	//String startdate= df.format(start);
+	Calendar c = Calendar.getInstance();
+	c.setTime(new Date());
+	c.add(Calendar.DATE, 1);
+	Member m1= new Member(3,"Murugan","Raga", "Vendren");
+	Facility f1 = new Facility("Basketball","Two basketball court are available");
+	
+	Booking bk = new Booking(start,start,m1,f1);
+	System.out.println(bk.toString());
 	}
 }
